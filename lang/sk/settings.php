705662fbd7a3c178672a1ca4f75bab7a0690fe41
Wizzard <wizzardsk@gmail.com>
2026-06-04 21:51:13 +0200

    Translation update (sk)

---
+++ lang/sk/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Wizzard <wizzardsk@gmail.com>
+ */
+$lang['email']                 = 'Zadajte prosím e-mailovú adresu, kde môžu používatelia požiadať o pomoc, keď sú zablokovaní neúmyselne.';
+$lang['skipblackhole']         = 'Bad Behavior predvolene kontroluje známe škodlivé IP v určitých realtime blacklistoch. Nevinní používatelia tak môžu byť zablokovaní kvôli predtým zneužitým dynamickým IP adresám. Zapnutím tejto voľby sa blackhole kontrola preskočí.';
